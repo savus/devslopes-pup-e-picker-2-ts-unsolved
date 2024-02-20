5b2392c0d72eb0676ec3c70169bd854616d58b91
Savus <justin.fuzzygreenstuff@gmail.com>
2024-02-20 05:33:44 -0700

    refactor api.ts

--- src/api.tsx
+++ src/api.tsx
@@ -3,42 +3,50 @@ import { Dog } from "./types";
 const BASE_URL = "http://localhost:3000";
 const endPoint = "dogs";
 
-export const Requests = {
-  getAllDogs: (): Promise<Dog[]> =>
+const getAllDogs = (): Promise<Dog[]> =>
+  //pessimistic rendering
+  fetch(`${BASE_URL}/${endPoint}`).then((response) => response.json());
+
+const postDog = (body: Omit<Dog, "id">) =>
+  fetch(`${BASE_URL}/${endPoint}`, {
     //pessimistic rendering
-    fetch(`${BASE_URL}/${endPoint}`).then((response) => response.json()),
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" },
+  }).then((response) => response.json());
 
-  postDog: (body: Omit<Dog, "id">) =>
-    fetch(`${BASE_URL}/${endPoint}`, {
-      //pessimistic rendering
-      method: "POST",
-      body: JSON.stringify(body),
-      headers: { "Content-Type": "application/json" },
-    }).then((response) => response.json()),
+const deleteDogRequest = (id: number): Promise<unknown> =>
+  //optimistic rendering
+  fetch(`${BASE_URL}/${endPoint}/${id}`, {
+    method: "DELETE",
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error(`Could not delete dog ${id}`);
+    } else {
+      return response.json();
+    }
+  });
 
-  deleteDogRequest: (id: number): Promise<unknown> =>
-    //optimistic rendering
-    fetch(`${BASE_URL}/${endPoint}/${id}`, {
-      method: "DELETE",
-    }).then((response) => {
-      if (!response.ok) {
-        throw new Error(`Could not delete dog ${id}`);
-      } else {
-        return response.json();
-      }
-    }),
+const patchFavoriteForDog = (
+  id: number,
+  body: Partial<Dog>
+): Promise<unknown> =>
+  //optimistic rendering
+  fetch(`${BASE_URL}/${endPoint}/${id}`, {
+    method: "PATCH",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" },
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error(`Could not patch dog ${id}`);
+    } else {
+      return response.json();
+    }
+  });
 
-  patchFavoriteForDog: (id: number, body: Partial<Dog>): Promise<unknown> =>
-    //optimistic rendering
-    fetch(`${BASE_URL}/${endPoint}/${id}`, {
-      method: "PATCH",
-      body: JSON.stringify(body),
-      headers: { "Content-Type": "application/json" },
-    }).then((response) => {
-      if (!response.ok) {
-        throw new Error(`Could not patch dog ${id}`);
-      } else {
-        return response.json();
-      }
-    }),
+export const Requests = {
+  getAllDogs,
+  postDog,
+  deleteDogRequest,
+  patchFavoriteForDog,
 };
